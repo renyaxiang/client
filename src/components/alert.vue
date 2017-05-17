ecc6141e15923801ed6c5beb42d8944bba7afe6a
xiangrenya <xiangrenya@gmail.com>
2017-05-17 15:57:28 +0800

    delete

--- src/components/alert.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="alert alert-danger">{{msg}}</div>
-</template>
-
-<script>
-  export default {
-    props: ['msg']
-  }
-</script>
--- src/components/loading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div  v-show="show" class="text-center text-muted">正在加载中...</div>
-</template>
-
-<script>
-  export default {
-    props: ['show']
-  }
-</script>
